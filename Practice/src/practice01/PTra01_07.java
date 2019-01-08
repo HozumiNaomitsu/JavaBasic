305cbedab763a14473620a2b5c1a0815a4e1a7bb
HozumiNaomitsu <46468857+HozumiNaomitsu@users.noreply.github.com>
2019-01-08 16:20:29 +0900

    １月８日

--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -22,23 +22,24 @@ public class PTra01_07 {
 		// ★ それぞれの型変数に、値を代入してください
 		// ※ 値は、好きな数字を入力してください
 
-
-
-
-
-
-
+bo=false;
+by=5;
+c='神';
+i=70;
+l=7000000;
+f=7.7F;
+d=3.3;
 
 
 		// ★ それぞれの変数の中身を出力してください
 
-
-
-
-
-
-
-
+System.out.println(bo);
+System.out.println(by);
+System.out.println(c);
+System.out.println(i);
+System.out.println(l);
+System.out.println(f);
+System.out.println(d);
 
 	}
-}
+	}
